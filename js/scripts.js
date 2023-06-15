360731b34dab5b15119ee3403bf044d2a6095481
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-06-15 17:56:47 +0200

    Dadi done

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,41 @@ function(event) {
  
     // Resetta il campo dell'email
     document.getElementById("email").value = "";
+
   });
  
+/* 
+    -----------------------------------------------------------------------------
+*/
+
+const form1 = document.getElementById('random-dadi');
+
+form1.addEventListener('submit',
+    function(evento) {
+        evento.preventDefault();
+    // Genera un numero casuale per il giocatore
+    let punteggioGiocatore = Math.round(Math.random() * 5) + 1;
+
+    // Genera un numero casuale per il computer
+    let punteggioComputer = Math.round(Math.random() * 5) + 1;
+
+    // Mostra i punteggi del giocatore e del computer
+    document.getElementById("punteggioGiocatore").innerHTML = punteggioGiocatore;
+    document.getElementById("punteggioComputer").innerHTML = punteggioComputer;
+
+    // Determina il vincitore
+    if (punteggioGiocatore > punteggioComputer) {
+      document.getElementById("risultato1").textContent = "Complimenti, hai vinto!";
+    } else if (punteggioGiocatore < punteggioComputer) {
+      document.getElementById("risultato1").textContent = "Hai perso. Riprova!";
+    } else {
+      document.getElementById("risultato1").textContent = "Pareggio. Giocate ancora!";
+    }
+  })
+
+
+
+
+
+
  
